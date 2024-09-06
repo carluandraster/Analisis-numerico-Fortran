419991cbbc4846f5df662b3c8af0fdfab116c29f
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-09-05 23:35:48 -0300

    Add files via upload

--- Guia 1/Ejercicio 14/ejercicio14.f90
+++ Guia 1/Ejercicio 14/ejercicio14.f90
@@ -33,4 +33,54 @@ contains
     end subroutine
 
     ! Grabar en un archivo una matriz de nxn
+    subroutine grabarMatriz (MATRIZ, N, ruta)
+        integer N,i,j
+        real(8) :: MATRIZ(N,N)
+        character(len=20) ruta
+        open(unit = 2,file = ruta);
+        write(2,'(I2)') N
+        do i = 1, N
+            do j = 1, N
+                write(2,'(F5.2)', advance='NO') MATRIZ(i,j)
+            end do
+            write(2,*)
+        end do
+        close(2)
+    end subroutine
+
+    ! Ingresar un vector de n componentes
+    subroutine ingresarVector (VECTOR, N, ruta)
+        integer N, ioError
+        real(8) :: VECTOR(N)
+        character(len=20) ruta
+        open(unit=3,file = ruta)
+        N = 0
+        read(3,'(F5.2)',ioStat = ioError) VECTOR(N+1)
+        do while (ioError == 0)
+            N = N+1
+            read(3,'(F5.2)',ioStat = ioError) VECTOR(N+1)
+        end do
+        close(3)
+    end subroutine
+
+    ! Imprimir por pantalla un vector de n componentes
+    subroutine imprimirVector(VECTOR,  N)
+        integer :: N,i
+        real(8) ::  VECTOR(N)
+        do i = 1, N
+            write(*,'(F5.2)',advance='NO') VECTOR(i)
+        end do
+    end subroutine imprimirVector
+
+    ! Grabar en un archivo un vector de n componentes
+    subroutine grabarVector(VECTOR,  N, ruta)
+        integer :: N,i
+        real(8) ::  VECTOR(N)
+        character(len=20) :: ruta
+        open(unit = 1, file = ruta)
+        do i = 1, N
+            write(1,'(F5.2)') VECTOR(N)
+        end do
+        close(1)
+    end subroutine grabarVector
 end module ejercicio14
