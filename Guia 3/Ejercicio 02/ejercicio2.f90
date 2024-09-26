523f089da5dcbd2174aedea12e6e0a02bec4e3f3
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-09-26 18:19:37 -0300

    Add files via upload

--- Guia 3/Ejercicio 02/ejercicio2.f90
+++ Guia 3/Ejercicio 02/ejercicio2.f90
@@ -1,19 +1,15 @@
 program ejercicio2
     implicit none
-    INTEGER,PARAMETER :: MAXELEM = 3
+    INTEGER,PARAMETER :: MAXELEM = 20
     REAL(8),DIMENSION(MAXELEM,MAXELEM) :: A,L,U
-    REAL(8),DIMENSION(MAXELEM,1) ::  B
-    INTEGER :: N,i,j
+    REAL(8),DIMENSION(MAXELEM,1) ::  B,C,X
+    INTEGER :: N
 
-    call leerArchivo(1,"prueba1.txt",A,B,N)
+    call leerArchivo(1,"incisoA.txt",A,B,N)
     call crout(A,L,U,N)
-    A = L*U
-    do i = 1, N
-        do j = 1, N
-            write(*,'(F8.4)',advance='NO') L(i,j)
-        end do
-        write(*,*)
-    end do
+    call primeraSolucion(B,L,C,N)
+    call solucionFinal(U,X,C,N)
+    call mostrarSolucion(X,N)
 
     contains
 
@@ -33,9 +29,10 @@ program ejercicio2
         close(unidad)
     end subroutine leerArchivo
 
-    subroutine crout(A,L,  U,N)
-        REAL(8) A(MAXELEM,MAXELEM),L(MAXELEM,MAXELEM),U(MAXELEM,MAXELEM)
+    subroutine crout(A,L,U,N)
+        REAL(8),DIMENSION(MAXELEM,MAXELEM) ::  A,L,U,VC
         INTEGER fila, col, i, k,N
+        VC = A
         do i = 1, N
             do k = i+1, n
                 L(i,k) = 0
@@ -43,7 +40,7 @@ program ejercicio2
             end do
         end do
         do i = 1, n
-            U(1,i) = A(1,i)/A(1,1)
+            A(1,i) = A(1,i)/A(1,1)
             U(i,i) = 1
             L(i,1) = A(i,1)
         end do
@@ -51,16 +48,62 @@ program ejercicio2
             ! Calcula la columna i
             DO fila=i, N
                 DO k=1,i-1
-                    L(fila,i)=A(fila,i) - A(fila,k)*A(k,i)
+                    A(fila,i)=A(fila,i) - A(fila,k)*A(k,i)
                 END DO
             END DO
             ! Calcula fila i
             DO col=i+1, N
                 DO k=1,i-1
-                    U(i, col)=A(i,col) - A(i,k)*A(k,col)
+                    A(i, col)=A(i,col) - A(i,k)*A(k,col)
                 END DO
-                U(i,col) = A(i,col) / A(i,i)
+                A(i,col) = A(i,col) / A(i,i)
             END DO
-        END DO 
+        END DO
+        do i = 1, N
+            do k = 1, N
+                if ( k<=i ) then
+                    L(i,k) = A(i,k)
+                else
+                    U(i,k) = A(i,k)
+                end if
+            end do
+        end do
+        A = VC
     end subroutine crout
+
+    subroutine primeraSolucion(B,L,C,N)
+        REAL(8) :: sumatoria
+        REAL(8),DIMENSION(MAXELEM,1) :: B,C
+        REAL(8),DIMENSION(MAXELEM,MAXELEM) :: L
+        INTEGER :: i,j,N
+        do i = 1, N
+            sumatoria = 0
+            do j = 1, i-1
+                sumatoria = sumatoria + L(i,j)*c(j,1)
+            end do
+            C(i,1) = (B(i,1)-sumatoria)/L(i,i)
+        end do
+    end subroutine primeraSolucion
+
+    subroutine solucionFinal(U,X,C,N)
+        REAL(8) :: sumatoria
+        REAL(8),DIMENSION(MAXELEM,1) :: X,C
+        REAL(8),DIMENSION(MAXELEM,MAXELEM) :: U
+        INTEGER :: i,j,N
+        do i = N, 1,-1
+            sumatoria = 0
+            do j = i+1, N
+                sumatoria = sumatoria+U(i,j)*X(j,1)
+            end do
+            X(i,1) = C(i,1)-sumatoria
+        end do
+    end subroutine solucionFinal
+
+    subroutine mostrarSolucion(X,N)
+        INTEGER :: N,i
+        REAL(8),DIMENSION(MAXELEM,1) :: X
+        do i = 1, N
+            write(*,'(A,I1,A,F10.6)') "x",i," = ",X(i,1)
+        end do
+    end subroutine mostrarSolucion
 end program ejercicio2
